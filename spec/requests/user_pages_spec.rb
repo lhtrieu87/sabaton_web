997f77cabecf45dca2cdeeca2892a5cc4d4fcf6c
Le Hong Trieu <pair+trieu@sabaton.com>
2013-08-12 00:15:15 +0800

    Add tests for redirected pages after failed/successful signing up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,6 +11,14 @@ describe "User pages" do
             it "should not create a user" do
                 expect {click_button submit}.not_to change(User, :count)
             end
+            describe "after submission" do
+                before {click_button submit}
+                
+                # Check redirected back to the Sign Up form
+                it {should have_title('Sign Up')}
+                # And error(s) shown
+                it {should have_content('error')}
+            end
         end
 
         describe "with valid information" do
@@ -23,6 +31,15 @@ describe "User pages" do
 
             it "should create a user" do
                 expect {click_button submit}.to change(User, :count).by(1)
+            end
+            
+            # Check redirected to the profile page
+            describe "after saving the user" do
+                before {click_button submit}
+                let(:user) {User.find_by(email: 'user@example.com')}
+        
+                it {should have_title(user.name)}
+                it {should have_selector('div.alert.alert-success', text: 'Welcome')}
             end
         end
     end
