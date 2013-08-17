a8cf722a085baa2e14168f46c0619e55274476eb
Le Hong Trieu <pair+trieu@sabaton.com>
2013-08-18 00:33:37 +0800

    Sign in out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,7 +14,7 @@ describe "Authentication" do
             it {should have_selector('div.alert.alert-error', text: 'Invalid')}
 
             describe "after visiting another page" do
-                before {click_link "Home"}
+                before {click_link "HOME"}
                 it {should_not have_selector('div.alert.alert-error')}
             end
         end
@@ -31,6 +31,11 @@ describe "Authentication" do
             it {should have_link('Profile', href: user_path(user))}
             it {should have_link('Sign out', href: signout_path)}
             it {should_not have_link('Sign in', href: signin_path)}
+
+            describe "followed by signout" do
+                before {click_link "Sign out"}
+                it {should have_link('SIGN IN')}
+            end
         end
     end
 end
