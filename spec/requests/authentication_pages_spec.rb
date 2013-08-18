f6a99a8c26ebe1daa664b7d4371169839ea4a3d1
Le Hong Trieu <pair+trieu@sabaton.com>
2013-08-18 18:45:46 +0800

    Test friendly forwarding to a default page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -53,6 +53,21 @@ describe "Authentication" do
                     it "should render the desired protected page" do
                         expect(page).to have_title('Edit User')
                     end
+                    
+                    describe "when signing in again" do
+                        before do
+                            # Sign out.
+                            delete signout_path
+                            visit signin_path
+                            fill_in "Email",    with: user.email
+                            fill_in "Password", with: user.password
+                            click_button "Sign in"
+                        end
+            
+                        it "should render the default (profile) page" do
+                            expect(page).to have_title(user.name)
+                        end
+                    end
                 end
             end
             
